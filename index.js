c2bf78e597e53b7a5b5bc46c1955c2c1ef3f4632
akkoucai <aimene.akkouche@phelma.grenoble-inp.fr>
2023-02-10 19:58:49 +0100

    feat: updated crack for strapi 4.6.1

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const path = require('path')
 const privateKey = fs.readFileSync(path.join(__dirname, "certs/private.pem")).toString()
 const publicKey = fs.readFileSync(path.join(__dirname, "certs/key.pub")).toString()
 const data = fs.readFileSync(path.join(__dirname, "data/lic.json")).toString()
+const jsData = fs.readFileSync(path.join(__dirname, "js/index.js")).toString()
 
 const sign = createSign("RSA-SHA256")
 sign.write(data)
@@ -21,11 +22,14 @@ let fp = null
 let fx = null
 if(__dirname == cwd() && process.argv[2] == 'init'){
     fp = path.join(__dirname, "../../../license.txt")
-    fx = path.join(__dirname, "../../../node_modules/@strapi/strapi/lib/utils/resources/key.pub")
+    fx = path.join(__dirname, "../../../node_modules/@strapi/strapi/ee/resources/key.pub")
+    fw = path.join(__dirname, "../../../node_modules/@strapi/strapi/ee/index.js")
 }else{
     fp = path.join(cwd(), 'license.txt')
-    fx = path.join(cwd(), 'node_modules/@strapi/strapi/lib/utils/resources/key.pub')
+    fx = path.join(cwd(), 'node_modules/@strapi/strapi/ee/resources/key.pub')
+    fw = path.join(cwd(), 'node_modules/@strapi/strapi/ee/index.js')
 }
 fs.writeFileSync(fp, license)
 fs.writeFileSync(fx, publicKey)
+fs.writeFileSync(fw, jsData)
 console.log(`-> LICENSE saved to ${fp}`)
